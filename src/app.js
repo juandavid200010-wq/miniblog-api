2b036f8f76948fcb604b1a7f4e58c66dc71f54a3
Juan David <juan.david200010@gmail.com>
2026-07-01 14:55:05 -0500

    feat: agrega middlewares de errorHandler y notFound

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-
 const authorsRoutes = require('./routes/authors.routes.js');
 const postsRoutes = require('./routes/posts.routes.js');
+const errorHandler = require('./middlewares/errorHandler.js');
+const notFound = require('./middlewares/notFound.js');
 
-app.use('/authors', authorsRoutes);
-app.use('/posts', postsRoutes);
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ message: 'Bienvenido a MiniBlog API' });
 });
+
+app.use('/authors', authorsRoutes);
+app.use('/posts', postsRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
